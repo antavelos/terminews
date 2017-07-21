9fcad481e1d2672059d7fb185797a8749a783d95
Alexandros Ntavelos <a.ntavelos@gmail.com>
2017-07-21 02:03:24 +0200

    setup basic functionality with gocui

--- ctrl/ctrl.go
+++ ctrl/ctrl.go
@@ -5,11 +5,17 @@ import (
 	"fmt"
 	"log"
 
-	// Both TUI packages are abbreviated to avoid making the code
-	// overly verbose.
-	"github.com/fatih/color"
+	"github.com/antavelos/terminews/db"
+	"github.com/antavelos/terminews/news"
+	"github.com/antavelos/terminews/rss"
+	"github.com/antavelos/terminews/ui"
 	c "github.com/jroimartin/gocui"
-	"github.com/pkg/errors"
+)
+
+const (
+	RSS_READERS_VIEW = "rssreaders"
+	NEWS_VIEW        = "news"
+	SUMMARY_VIEW     = "summary"
 )
 
 // Items to fill the list with.
@@ -21,13 +27,20 @@ var listItems = []string{
 	"Line 5",
 }
 
-// var tdb *db.TDB
-var err error
+var (
+	tdb      *db.TDB
+	g        *c.Gui
+	err      error
+	rrList   *ui.List
+	newsList *ui.List
+	summary  *c.View
+)
 
-// func handleDBFatalError(err error) {
-// 	tdb.Close()
-// 	log.Fatal(err)
-// }
+func handleFatalError(msg string, err error) {
+	tdb.Close()
+	g.Close()
+	log.Fatal(msg, err)
+}
 
 func spaces(n int) string {
 	var s bytes.Buffer
@@ -38,111 +51,134 @@ func spaces(n int) string {
 
 }
 
-// Set up the widgets and run the event loop.
-func Main() {
-	// Create a new GUI.
-	g, err := c.NewGui(c.OutputNormal)
-	if err != nil {
-		log.Println("Failed to create a GUI:", err)
-		return
-	}
-	defer g.Close()
-
-	// g.Cursor = true
-
-	g.SetManagerFunc(layout)
-
-	err = g.SetKeybinding("", c.KeyCtrlC, c.ModNone, quit)
-	if err != nil {
-		log.Println("Could not set key binding:", err)
-		return
-	}
-
+func CreateViews() {
 	tw, th := g.Size()
 
 	lw := (tw * 3) / 10
 	oh := (th * 6) / 10
 
-	lv, err := g.SetView("rssreaders", 0, 0, lw, th-1)
-	if err != nil && err != c.ErrUnknownView {
-		log.Println("Failed to create rssreaders view:", err)
-		return
-	}
-	wb := color.New(color.FgBlack, color.BgWhite)
-
-	lv.Title = " RSS Readers "
-	lv.FgColor = c.ColorCyan
-
-	// Then the output view.
-	ov, err := g.SetView("news", lw+1, 0, tw-1, oh)
-	if err != nil && err != c.ErrUnknownView {
-		log.Println("Failed to create news view:", err)
-		return
-	}
-	ov.Title = "News from ..."
-	ov.FgColor = c.ColorGreen
-	// Let the view scroll if the output exceeds the visible area.
-	ov.Autoscroll = true
-	ov.Wrap = true
-
-	// And finally the input view.
-	iv, err := g.SetView("summary", lw+1, oh+1, tw-1, th-1)
-	if err != nil && err != c.ErrUnknownView {
-		log.Println("Failed to create summary view:", err)
-		return
-	}
-	iv.Title = "Summary"
-	iv.FgColor = c.ColorYellow
-	// The input view shall be editable.
-
-	// Make the enter key copy the input to the output.
-	err = g.SetKeybinding("input", c.KeyEnter, c.ModNone, func(g *c.Gui, iv *c.View) error {
-		// We want to read the view's buffer from the beginning.
-		iv.Rewind()
-
-		// Get the output view via its name.
-		ov, e := g.View("output")
-		if e != nil {
-			log.Println("Cannot get output view:", e)
-			return e
-		}
-		// Thanks to views being an io.Writer, we can simply Fprint to a view.
-		_, e = fmt.Fprint(ov, iv.Buffer())
-		if e != nil {
-			log.Println("Cannot print to output view:", e)
-		}
-		// Clear the input view
-		iv.Clear()
-		// Put the cursor back to the start.
-		e = iv.SetCursor(0, 0)
-		if e != nil {
-			log.Println("Failed to set cursor:", e)
-		}
-		return e
-
-	})
+	// RSS Readers List
+	rrList, err = ui.CreateList(g, RSS_READERS_VIEW, 0, 0, lw, th-1)
 	if err != nil {
-		log.Println("Cannot bind the enter key:", err)
+		handleFatalError("Failed to create rssreaders list:", err)
+	}
+	rrList.Title = " RSS Readers "
+
+	//
+	newsList, err = ui.CreateList(g, NEWS_VIEW, lw+1, 0, tw-1, oh)
+	if err != nil {
+		handleFatalError(" Failed to create news list:", err)
+	}
+	newsList.Title = "News from ..."
+
+	// Summary view
+	summary, err = g.SetView(SUMMARY_VIEW, lw+1, oh+1, tw-1, th-1)
+	if err != nil && err != c.ErrUnknownView {
+		handleFatalError("Failed to create summary view:", err)
+	}
+	summary.Title = " Summary "
+	summary.Wrap = true
+}
+
+func UpdateSummary(event news.Event) {
+	summary.Clear()
+	fmt.Fprintf(summary, "By %v\n", event.Author)
+	fmt.Fprintf(summary, "Published on %v\n\n", event.Published)
+	fmt.Fprintf(summary, "%v", event.Description)
+}
+
+func UpdateNews(rr db.RssReader) {
+	events, err := rss.Retrieve(rr.Url)
+	if err != nil {
+		newsList.Title = fmt.Sprintf(" Failed to load news from %v ", rr.Name)
+		newsList.Clear()
+	}
+	var data []ui.Displayer = make([]ui.Displayer, len(events))
+	for i, e := range events {
+		data[i] = e
 	}
 
-	// Fill the list view.
-	for _, s := range listItems {
-		// Again, we can simply Fprint to a view.
-		_, err = wb.Fprintln(lv, s)
-		if err != nil {
-			log.Println("Error writing to the list view:", err)
-			return
-		}
+	if err = newsList.SetItems(data); err != nil {
+		handleFatalError("Failed to update news list", err)
 	}
-	// Set the focus to the input view.
-	_, err = g.SetCurrentView("rssreaders")
+	newsList.Title = fmt.Sprintf(" News from %v ", rr.Name)
+	newsList.Focus(g)
+
+	summary.Clear()
+	fmt.Fprintf(summary, "By %v\n", events[0].Author)
+	fmt.Fprintf(summary, "Published on %v\n\n", events[0].Published)
+	fmt.Fprintf(summary, "%v", events[0].Description)
+}
+
+func LoadRssReaders() []db.RssReader {
+	rssReaders, err := tdb.GetRssReaders()
 	if err != nil {
-		log.Println("Cannot set focus to input view:", err)
+		handleFatalError("Failed to load RSS Readers", err)
+	}
+	var data []ui.Displayer = make([]ui.Displayer, len(rssReaders))
+	for i, rr := range rssReaders {
+		data[i] = rr
+	}
+
+	if err = rrList.SetItems(data); err != nil {
+		handleFatalError("Failed to update rss readers list", err)
+	}
+
+	return rssReaders
+}
+
+func InitUI() {
+	// Create a new GUI.
+	g, err = c.NewGui(c.OutputNormal)
+	if err != nil {
+		handleFatalError("Failed to initialize GUI", err)
+	}
+
+	// g.Cursor = true
+	g.SelFgColor = c.ColorGreen
+	g.Highlight = true
+
+	g.SetManagerFunc(layout)
+}
+
+func Free() {
+	g.Close()
+}
+
+// Set up the widgets and run the event loop.
+func Main() {
+	// Init DB
+	if tdb, err = db.InitDB("./terminews.db"); err != nil {
+		tdb.Close()
+		log.Fatal(err)
+	}
+
+	InitUI()
+	defer Free()
+
+	CreateViews()
+
+	rssReaders := LoadRssReaders()
+
+	UpdateNews(rssReaders[0])
+
+	err = g.SetKeybinding("", c.KeyCtrlC, c.ModNone, quit)
+	if err != nil {
+		handleFatalError("Could not set key binding:", err)
+	}
+
+	err = g.SetKeybinding("", c.KeyCtrlC, c.ModNone, quit)
+	if err != nil {
+		handleFatalError("Could not set key binding:", err)
+	}
+
+	err = g.SetKeybinding("", c.KeyTab, c.ModNone, switchView)
+	if err != nil {
+		handleFatalError("Could not set key binding:", err)
 	}
 
 	// Start the main loop.
-	err = g.MainLoop()
-	log.Println("Main loop has finished:", err)
+	g.MainLoop()
 }
 
 // The layout handler calculates all sizes depending
@@ -154,17 +190,17 @@ func layout(g *c.Gui) error {
 	lw := (tw * 3) / 10
 	oh := (th * 6) / 10
 
-	_, err := g.SetView("rssreaders", 0, 0, lw, th-1)
+	_, err := g.SetView(RSS_READERS_VIEW, 0, 0, lw, th-1)
 	if err != nil {
-		return errors.Wrap(err, "Cannot update list view")
+		handleFatalError("Cannot update list view", err)
 	}
-	_, err = g.SetView("news", lw+1, 0, tw-1, oh)
+	_, err = g.SetView(NEWS_VIEW, lw+1, 0, tw-1, oh)
 	if err != nil {
-		return errors.Wrap(err, "Cannot update output view")
+		handleFatalError("Cannot update output view", err)
 	}
-	_, err = g.SetView("summary", lw+1, oh+1, tw-1, th-1)
+	_, err = g.SetView(SUMMARY_VIEW, lw+1, oh+1, tw-1, th-1)
 	if err != nil {
-		return errors.Wrap(err, "Cannot update input view.")
+		handleFatalError("Cannot update input view.", err)
 	}
 	return nil
 }
@@ -173,4 +209,13 @@ func layout(g *c.Gui) error {
 // It signals the main loop to exit.
 func quit(g *c.Gui, v *c.View) error {
 	return c.ErrQuit
+}
+
+func switchView(g *c.Gui, v *c.View) error {
+	if g.CurrentView() == rrList.View {
+		newsList.Focus(g)
+	} else {
+		rrList.Focus(g)
+	}
+	return nil
 }
--- ui/ui.go
+++ ui/ui.go
@@ -2,126 +2,50 @@ package ui
 
 import (
 	"fmt"
-	gc "github.com/rthornton128/goncurses"
+	c "github.com/jroimartin/gocui"
 )
 
 type Displayer interface {
 	Display() string
 }
 
-type TMenuItem struct {
-	*gc.MenuItem
-	inc  int
+type List struct {
+	*c.View
+	name string
 	Data Displayer
 }
 
-func (tmi *TMenuItem) Create(inc int, data Displayer) error {
-	value := fmt.Sprintf(`%3d. %+q`, inc, data.Display())
-	item, err := gc.NewItem(value, "")
-	if err != nil {
-		return err
-	}
-	tmi.inc = inc
-	tmi.Data = data
-	tmi.MenuItem = item
-
-	return nil
-}
-
-type TMenu struct {
-	*gc.Menu
-	items []*TMenuItem
-}
-
-func (tm *TMenu) Create() error {
-	menu, err := gc.NewMenu([]*gc.MenuItem{})
-	if err != nil {
-		return err
-	}
-	tm.Menu = menu
-
-	return nil
-}
-
-func (tm *TMenu) RefreshItems(dataItems []Displayer) ([]*TMenuItem, error) {
-	var tmis []*TMenuItem
-	var gcItems []*gc.MenuItem
-
-	for _, gcmi := range tm.Items() {
-		gcmi.Free()
-	}
-
-	for i, data := range dataItems {
-		tmi := &TMenuItem{}
-		if err := tmi.Create(i+1, data); err != nil {
-			return nil, err
-		}
-		tmis = append(tmis, tmi)
-	}
-	for _, tmi := range tmis {
-		gcItems = append(gcItems, tmi.MenuItem)
-	}
-	if err := tm.SetItems(gcItems); err != nil {
+func CreateList(g *c.Gui, name string, x0, y0, x1, y1 int) (*List, error) {
+	v, err := g.SetView(name, x0, y0, x1, y1)
+	if err != nil && err != c.ErrUnknownView {
 		return nil, err
 	}
-	tm.items = tmis
 
-	return tmis, nil
+	list := &List{}
+	list.View = v
+	list.name = name
+	list.SelBgColor = c.ColorWhite
+	list.SelFgColor = c.ColorBlack
+	list.BgColor = c.ColorBlack
+	list.Autoscroll = true
+
+	return list, nil
 }
 
-type TWindow struct {
-	*gc.Window
-	title      string
-	H, W, Y, X int
-}
-
-func (tw *TWindow) Create(title string, h, w, y, x int) error {
-	win, err := gc.NewWindow(h, w, y, x)
-	if err != nil {
+func (l *List) Focus(g *c.Gui) error {
+	if _, err := g.SetCurrentView(l.name); err != nil {
 		return err
 	}
-	tw.Window = win
-	tw.H = h
-	tw.W = w
-	tw.Y = y
-	tw.X = x
-
-	tw.Keypad(true)
-	tw.Box(0, 0)
-	tw.SetHLine(2)
-	tw.SetTitle(title)
-
 	return nil
 }
 
-func (tw *TWindow) SetTitle(title string) {
-	tw.title = title
-	tw.SetLine(title, 1, (tw.W/2)-(len(title)/2))
-}
+func (l *List) SetItems(items []Displayer) error {
+	l.Clear()
+	for i, item := range items {
+		if _, err := fmt.Fprintf(l.View, "%2d. %v\n", i+1, item.Display()); err != nil {
+			return err
+		}
+	}
 
-func (tw *TWindow) SetHLine(y int) {
-	tw.HLine(y, 1, gc.ACS_HLINE, tw.W-2)
-}
-
-func (tw *TWindow) SetLine(s string, y, x int) {
-	tw.MovePrint(y, x, s)
-}
-
-func (tw *TWindow) Focus(cc int16) {
-	tw.ColorOn(cc)
-	tw.Box(0, 0)
-	tw.ColorOff(cc)
-}
-
-func (tw *TWindow) Unfocus(cc int16) {
-	tw.ColorOn(cc)
-	tw.Box(0, 0)
-	tw.ColorOff(cc)
-}
-
-func (tw *TWindow) AttachMenu(tm *TMenu) {
-	tm.Menu.SetWindow(tw.Window)
-	tm.Menu.SubWindow(tw.Derived(tw.H-4, tw.W-4, 4, 2))
-	tm.Menu.Format(tw.H-4, 1)
-	tm.Menu.Mark("")
+	return nil
 }
